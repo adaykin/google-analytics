748dcbf5ef64d0e91b8db57c46f8a6926d511cb8
Andy Daykin <daykinandy@gmail.com>
2012-03-15 23:17:58 -0500

    Updated names of settings and parameters for more descriptive names. Fixed validation and display.

--- google-analytics-admin.php
+++ google-analytics-admin.php
@@ -1,31 +1,33 @@
 <?php 
 
-add_action('admin_menu', 'google_analytics_admin_add_page');
-add_action('admin_init', 'google_analytics_admin_init');
+add_action('admin_menu', 'google_analytics_admin_page');
+add_filter('plugin_action_links', 'google_analytics_plugin_action_links', 10, 2);
+
 
 function google_analytics_admin_init()
 {
-	register_setting('plugin_options', 'plugin_options', 'google_analytics_options_validate');
-	add_settings_section('plugin_main', 'Main Settings', 'google_analytics_section_text', 'plugin');
-	add_settings_field('plugin_text_string', 'Plugin Text Input', 'google_analytics_setting_string', 'plugin', 'plugin_main');
+	register_setting('plugin_options', 'analytics_code', 'google_analytics_options_validate');
 }
 
 function google_analytics_section_text()
 {
-	echo '<p>Main description of this section here.</p>';
+	echo '<p>Enter your code given to you by google analytics in the field below.</p>';
 }
 
-function google_analytics_admin_add_page()
+function google_analytics_admin_page()
 {
-	add_options_page('Custom Plugin Page', 'Custom Plugin Menu', 'manage_options', 'plugin', 'google_analytics_options_page');
+	add_options_page('Google Analytics Plugin Settings', 'Google Analytics', 'manage_options', 'google-analytics-options', 'google_analytics_options_page');
+	add_settings_section('plugin_main', 'Code', 'google_analytics_section_text', 'plugin');
+	add_settings_field('analytics_code_display', 'Google Analytics Code', 'google_analytics_setting_string', 'plugin', 'plugin_main');
+	
+	add_action('admin_init', 'google_analytics_admin_init');
 }
 
 function google_analytics_options_page()
 {
 ?>
 	<div>
-	<h2>My custom plugin</h2>
-	Options relating to the Custom Plugin.
+	<h2>Google Analytics</h2>
 	<form action="options.php" method="post">
 	<?php settings_fields('plugin_options'); ?>
 	<?php do_settings_sections('plugin'); ?>
@@ -37,15 +39,26 @@ function google_analytics_options_page()
 
 function google_analytics_setting_string()
 {
-	$options = get_option('plugin_options');
-	echo "<textarea id='plugin_text_string' name='plugin_options[text_string]' cols='40' rows='60'>{$options['text_string']}</textarea>";
+	$options = get_option('analytics_code');
+	echo "<textarea id='google_analytics_code' name='analytics_code[google_analytics_code]' cols='100' rows='10'>{$options['google_analytics_code']}</textarea>";
 }
 
 function google_analytics_options_validate($input)
 {
-	$newinput['text_string'] = trim($input['text_string']);
-	if(!preg_match('/^[a-z0-9]{32}$/i', $newinput['text_string'])) {
-		$newinput['text_string'] = '';
-	}
+	$newinput['google_analytics_code'] = trim($input['google_analytics_code']);
+	// Later on add some better validation
+	/*if(!preg_match('/[a-z0-9]/i', $newinput['google_analytics_code'])) {
+		$newinput['google_analytics_code'] = '';
+	}*/
+	
 	return $newinput;
+}
+
+function google_analytics_plugin_action_links($links, $file)
+{
+	if($file == plugin_basename(dirname(__FILE__) . '/google-analytics.php')) {
+		$links[] = '<a href="options-general.php?page=google-analytics-admin.php">'.__('Settings').'</a>';
+	}
+
+	return $links;
 }
--- google-analytics.php
+++ google-analytics.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Google Analytics Management
-Plugin URI: http://andydaykin.com
+Plugin URI: https://github.com/adaykin/google-analytics
 Description: Management for google analytics placement
 Version: 0.1
 Author: Andy Daykin
@@ -12,22 +12,14 @@ License: MIT
 add_action('wp_footer', 'outputAnalytics');
 
 if(is_admin()) {
-	add_filter('plugin_action_links', 'google_analytics_plugin_action_links', 10, 2);
+	require_once 'google-analytics-admin.php';
 }
 
 function outputAnalytics()
 {
-	$option = get_option('plugin_options');
-	echo $options['text_string'];
-}
-
-function google_analytics_plugin_action_links($links, $file)
-{
-	if($file == plugin_basename(dirname(__FILE__) . '/google-analytics.php')) {
-		$links[] = '<a href="options-general.php?page=google-analytics-admin.php">'.__('Settings').'</a>';
-	}
-
-	return $links;
+	$options = get_option('analytics_code');
+	error_log("Output: " . $options['google_analytics_code']);
+	echo $options['google_analytics_code'];
 }
 
 ?>
